f108ced5e39848e7243b622fafeb60a3a19ff5ff
orcax <siriusxyk@gmail.com>
2016-10-20 20:45:51 -0400

    update preprocess

---
+++ dqn/preprocess.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+import glob
+import os
+import os.path
+import sys
+import cv2
+import numpy as np
+sys.path.append('/home/yx150/Documents/nips2015-action-conditional-video-prediction/caffe/python')
+import caffe
+from caffe.io import array_to_blobproto
+
+def create_act_idx(data_dir):
+    print 'create action index...'
+    act_map = {}
+    for d in glob.glob(data_dir + '/*'):
+        act_file = d + '/act.log'
+        act_idx_file = d + '/act_idx.log'
+        if not os.path.exists(act_file):
+            continue
+        acts = []
+        with open(act_file, 'r') as f:
+            acts = [int(l.strip()) for l in f.readlines()]
+        if not act_map:
+            act_key = sorted(list(set(acts)))
+            act_val = range(len(act_key))
+            act_map = dict(zip(act_key, act_val))
+        with open(act_idx_file, 'w') as f:
+            f.writelines([str(act_map[a])+'\n' for a in acts])
+
+    with open(data_dir + '/act_map.log', 'w') as f:
+        tmp = [(act_map[a], a) for a in act_map]
+        tmp = sorted(tmp, key=lambda x:x[0])
+        for i in range(len(tmp)):
+            line = str(tmp[i][0]) + ',' + str(tmp[i][1]) + '\n'
+            f.write(line)
+
+def compute_mean(data_dir):
+    print 'compute mean file...'
+    sumfile = np.zeros((210, 160, 3), dtype=np.float)
+    count = 0
+    for d in glob.glob(data_dir + '/*'):
+        for im in glob.glob(d + '/*.png'):
+            im = cv2.imread(im, cv2.IMREAD_COLOR)
+            im = im / 255.0
+            sumfile += im
+            count += 1
+
+    meanfile = sumfile / count
+    np.save(data_dir + '/mean.npy', meanfile * 255.0)
+    cv2.imwrite(data_dir + '/mean.png', meanfile * 255.0)
+    meanblob = meanfile.transpose((2,0,1))
+    meanblob = array_to_blobproto(meanblob)
+    with open(data_dir + '/mean.binaryproto', 'wb') as f:
+        f.write(meanblob.SerializeToString())
+
+def main():
+    assert len(sys.argv) > 1
+    game = sys.argv[1]
+    create_act_idx('./%s/train' % game)
+    create_act_idx('./%s/test' % game)
+    compute_mean('./%s/train' % game)
+
+if __name__ == '__main__':
+    main()
